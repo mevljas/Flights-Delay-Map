8b24a81dd3f37747b8fc2545c0e14fc0e65f362d
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2022-10-25 08:46:02 +0200

    fix  tsc watch

--- Seminars/1.seminar/airline-delay/src/draw.ts
+++
@@ -1,6 +0,0 @@
-import type p5 from "p5";
-
-/** This is a draw function. */
-export const draw = (p: p5): void => {
-  p.background(220);
-};
---
+++ Seminars/1.seminar/airline-delay/src/script.ts
@@ -0,0 +1,9 @@
+/** This is a setup function. */
+function setup() {
+  createCanvas(800, 400)
+}
+
+/** This is a draw function. */
+function draw() {
+  background(220)
+}
