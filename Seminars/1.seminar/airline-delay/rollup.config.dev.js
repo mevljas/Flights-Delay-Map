a5c29f7c155ae28a667592df200f28008d6084ce
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2022-10-25 11:06:34 +0200

    fix: watch project

--- Seminars/1.seminar/airline-delay/rollup.config.dev.js
+++ Seminars/1.seminar/airline-delay/rollup.config.dev.js
@@ -1,4 +1,7 @@
 // See also: https://rollupjs.org/
+import typescript from 'rollup-plugin-typescript2'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
 
 const banner = `/**
  * This is a p5.js sketch made with p5js-template-petr-plus.
@@ -16,7 +19,15 @@ const config = {
     globals: {p5: 'p5'},
     interop: 'default'
   },
-  external: ['p5']
+  external: ['p5'],
+  plugins: [
+    typescript(),
+    serve('dist'),
+    livereload({
+      watch: ['dist', 'src'],
+      exts: ['html', 'js', 'scss', 'sass', 'css']
+    })
+  ]
 }
 
 export default config
