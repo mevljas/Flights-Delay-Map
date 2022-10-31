858502114315465cfb6e35ad8d3a6378d5017c5f
Sebastjan Mevlja <sm9299@student.uni-lj.si>
2022-10-31 14:53:07 +0100

    fix build errors

--- Seminars/1.seminar/airline-delay/rollup.config.js
+++ Seminars/1.seminar/airline-delay/rollup.config.js
@@ -1,4 +1,6 @@
 // See also: https://rollupjs.org/
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 const config = {
   input: 'tsc-out/main.js',
@@ -8,7 +10,11 @@ const config = {
     globals: {p5: 'p5', bootstrap: 'bootstrap', mapboxgl: 'mapboxgl'},
     interop: 'default'
   },
-  external: ['p5', 'bootstrap', 'mapboxgl']
+  external: ['p5', 'bootstrap', 'mapboxgl'],
+  plugins: [
+    resolve(), // so Rollup can find `mapbox-gl`
+    commonjs()
+  ]
 }
 
 export default config
