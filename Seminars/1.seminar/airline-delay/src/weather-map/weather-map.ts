4a243371a4ab7d2bdca54088ccbce324a62b97bf
Sebastjan Mevlja <sm9299@student.uni-lj.si>
2022-10-31 14:53:17 +0100

    refactor map

--- Seminars/1.seminar/airline-delay/src/weather-map/weather-map.ts
+++ Seminars/1.seminar/airline-delay/src/weather-map/weather-map.ts
@@ -2,34 +2,31 @@ import bootstrap from 'bootstrap'
 import mapboxgl from 'mapbox-gl'
 
 export class WeatherMap {
-  readonly msPerMinute: number = 60000 //offset in milliseconds
-  readonly currentDateTime: string = new Date().toISOString().slice(0, -8)!
-  dateButton: HTMLElement = document.getElementById('openDataPicker')!
-  submitButton: HTMLElement = document.getElementById('unixTsSubmit')!
-  currentTime: Date = new Date()
-  currentTimeStamp = this.getTimeStamp(this.currentTime)
-  dateModal = new bootstrap.Modal(document.getElementById('tsDialog')!, {
-    keyboard: false
-  })
-  weatherCheckbox: HTMLInputElement = document.querySelector(
+  readonly mapLayerName: string = 'weather-radar-layer'
+  readonly currentDateTime: string
+  readonly openDateModalButton: HTMLElement =
+    document.getElementById('openDataPicker')!
+  readonly submiDateButton: HTMLElement =
+    document.getElementById('unixTsSubmit')!
+  readonly dateTimePicker: HTMLInputElement = document.getElementById(
+    'tsDateTime'
+  ) as HTMLInputElement
+  readonly startTime
+  readonly dateModal
+  readonly weatherCheckbox: HTMLInputElement = document.querySelector(
     'input[id=weatherCheckbox]'
   )!
-  allFrames!: number
-  radarLoopTime!: number
-  minutesToSubtract: number[] = []
   map: mapboxgl.Map
 
   constructor() {
-    ;(<HTMLInputElement>document.getElementById('tsDateTime'))!.value =
-      this.currentDateTime
-
-    this.dateButton.innerHTML = this.printFancyTime(
-      (<HTMLInputElement>document.getElementById('tsDateTime'))!.value
-    )
-
-    this.numberOfFrames(15)
-    this.loopSpeed(500)
-
+    this.currentDateTime = new Date().toISOString().slice(0, -8)!
+    this.dateTimePicker.value = this.currentDateTime
+    this.startTime = this.parseTimeToSeconds(new Date())
+    this.dateModal = new bootstrap.Modal(document.getElementById('tsDialog')!, {
+      keyboard: false
+    })
+    this.syncDateButtonValue()
+    this.registerEventListeners()
     mapboxgl.accessToken =
       'pk.eyJ1IjoibWV2bGphcyIsImEiOiJjbDlyaXRsdDkwNXV6M3ZxZ2loNGUwNDBoIn0.c0FcsLrkv_Hlrj4OnIqmSA'
     this.map = new mapboxgl.Map({
@@ -49,96 +46,73 @@ export class WeatherMap {
     this.map.on('load', () => {
       // Set the default atmosphere style
       this.map.setFog({})
-      this.weatherCheckbox.addEventListener('change', () => {
-        if (this.weatherCheckbox.checked) {
-          this.initializeNexradLayer(this.allFrames)
-        } else {
-          this.removeNexradLayer(this.allFrames + 1)
-        }
-      })
-      this.submitButton.addEventListener('click', () => {
-        this.setnewDateTime()
-      })
+      this.addWeatherRadarLayer()
     })
   }
 
-  printFancyTime(date: string) {
+  registerEventListeners() {
+    this.weatherCheckbox.addEventListener('change', () => {
+      this.updateWeatherLayer()
+    })
+    this.submiDateButton.addEventListener('click', () => {
+      this.setNewDateTime()
+      this.updateWeatherLayer()
+    })
+  }
+
+  updateWeatherLayer() {
+    this.removeWeatherRadarLayer()
+    if (this.weatherCheckbox.checked) {
+      this.addWeatherRadarLayer()
+    }
+  }
+
+  // Parses date into a nice format.
+  parseDateTime(date: string) {
     // 31/10/2022 14:45:39
     return date.replaceAll('-', '/').replace('T', ' ')
   }
 
-  getTimeStamp(dateObje: Date) {
+  parseTimeToSeconds(dateObje: Date) {
     return Math.round(dateObje.getTime() / 1000)
   }
 
-  numberOfFrames(numofframes: number) {
-    this.allFrames = numofframes
-    for (var w = 0; w < numofframes + 1; w++) {
-      this.minutesToSubtract.push(w * 5)
-    }
-    this.minutesToSubtract.reverse()
-  }
-  loopSpeed(timeMs: number) {
-    // radar loop time is in ms
-    this.radarLoopTime = timeMs
+  syncDateButtonValue() {
+    this.openDateModalButton.innerHTML = this.parseDateTime(
+      this.dateTimePicker.value
+    )
   }
 
-  setnewDateTime() {
-    var inputtedTs = (<HTMLInputElement>document.getElementById('tsDateTime'))!
-      .value
-    console.log(inputtedTs)
-    this.dateButton.innerHTML = this.printFancyTime(
-      (<HTMLInputElement>document.getElementById('tsDateTime')).value
-    )
+  setNewDateTime() {
+    console.log(this.dateTimePicker.value)
+    this.syncDateButtonValue()
     this.dateModal.toggle()
   }
-  initializeNexradLayer(fram: number) {
-    for (var t = fram; t > -1; t--) {
-      if (t == fram) {
-        this.addNexradLayer(t, 1)
-      } else {
-        this.addNexradLayer(t, 0)
-      }
+  removeWeatherRadarLayer() {
+    if (this.map.getLayer(this.mapLayerName)) {
+      this.map.removeLayer(this.mapLayerName)
     }
-  }
-  removeNexradLayer(frames: number) {
-    for (var t = 0; t < frames; t++) {
-      if (this.map.getLayer(`wms-test-layer${t}`)) {
-        this.map.removeLayer(`wms-test-layer${t}`)
-      }
-      if (this.map.getSource(`wms-test-layer${t}`)) {
-        this.map.removeSource(`wms-test-layer${t}`)
-      }
+    if (this.map.getSource(this.mapLayerName)) {
+      this.map.removeSource(this.mapLayerName)
     }
   }
 
-  addNexradLayer(indexInArray: number, opacity: number) {
-    var inputtedTs = (<HTMLInputElement>document.getElementById('tsDateTime'))
-      .value
-    var tsTime = new Date(inputtedTs)
-    var subtractedTime = new Date(
-      tsTime.getTime() -
-        this.minutesToSubtract[indexInArray]! * this.msPerMinute
-    )
+  addWeatherRadarLayer() {
+    let inputtedTs = this.dateTimePicker.value
+    let tsTime = new Date(inputtedTs)
+    let timestampt = this.generateWeatherRadarTimestamp(tsTime)
     this.map.addLayer({
-      id: `wms-test-layer${indexInArray}`,
+      id: this.mapLayerName,
       type: 'raster',
       source: {
         type: 'raster',
-        // use the tiles option to specify a WMS tile source URL
-        // https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/
         tiles: [
-          `https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/ridge::USCOMP-N0Q-${this.isuTimestamp(
-            subtractedTime
-          )}/{z}/{x}/{y}.png`
-          //'https://img.nj.gov/imagerywms/Natural2015?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=Natural2015'
-          //'https://opengeo.ncep.noaa.gov/geoserver/klwx/klwx_bref_raw/ows?&service=WMS&request=GetMap&layers=&styles=&format=image/png&transparent=true&version=1.1.1&SERVICE=WMS&LAYERS=klwx_bref_raw&width=256&height=256&srs=EPSG:3857&bbox={bbox-epsg-3857}'
-          //https://opengeo.ncep.noaa.gov/geoserver/klwx/klwx_bref_raw/ows?&service=WMS&request=GetMap&layers=&styles=&format=image/png&transparent=true&version=1.1.1&SERVICE=WMS&LAYERS=klwx_bref_raw&width=256&height=256&srs=EPSG:3857&bbox=-20037508.342789244,0,-10018754.171394622,10018754.17139462
+          `https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/ridge::USCOMP-N0Q-${timestampt}/{z}/{x}/{y}.png`
         ],
         tileSize: 256
       },
       paint: {
-        'raster-opacity': opacity,
+        'raster-opacity': 1,
         'raster-opacity-transition': {
           duration: 0,
           delay: 0
@@ -146,12 +120,10 @@ export class WeatherMap {
         'raster-fade-duration': 0
       }
     })
-    console.log(
-      `added layer ${indexInArray} - ${this.isuTimestamp(subtractedTime)}`
-    )
+    console.log(`New weather radar layer ${timestampt}`)
   }
 
-  isuTimestamp(date: Date) {
+  generateWeatherRadarTimestamp(date: Date) {
     return (
       date.getFullYear() +
       ('0' + (date.getUTCMonth() + 1)).slice(-2) +
